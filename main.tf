fff6eb7e865400a00ab0ff9b822bf3e0eda99b79
Michael Dimitras <michael.dimitras@p0.dev>
2026-02-11 10:51:20 -0800

    Add routing rules

--- main.tf
+++ main.tf
@@ -182,3 +182,129 @@ module "aws_p0_ssm_documents_us_west_2" {
   }
   source = "./modules/aws_p0_ssm_documents"
 }
+
+/**********************************
+  P0 routing rules
+**********************************/
+
+resource "p0_routing_rule" "aws_any_request_requires_p0_approval" {
+  name = "aws-any-request-requires-p0-approval"
+
+  requestor = {
+    type = "any"
+  }
+
+  resource = {
+    type    = "integration"
+    service = "aws"
+  }
+
+  approval = [{
+    type = "p0"
+  }]
+}
+
+resource "p0_routing_rule" "ssh_any_request_requires_p0_approval_with_duration" {
+  name = "ssh-any-request-requires-p0-approval-with-duration"
+
+  requestor = {
+    type = "any"
+  }
+
+  resource = {
+    type        = "integration"
+    service     = "ssh"
+    access_type = "all"
+  }
+
+  approval = [{
+    type = "p0"
+    options = {
+      require_duration = true
+    }
+  }]
+}
+
+resource "p0_routing_rule" "okta_manager_approval" {
+  name = "example-okta-manager-approval"
+
+  requestor = {
+    type = "any"
+  }
+
+  resource = {
+    type = "any"
+  }
+
+  approval = [{
+    type             = "requestor-profile"
+    directory        = "okta"
+    profile_property = "manager"
+    options = {
+      require_reason = true
+    }
+  }]
+}
+
+resource "p0_routing_rule" "okta_group_approval" {
+  name = "example-okta-group-approval"
+
+  requestor = {
+    type = "any"
+  }
+
+  resource = {
+    type = "any"
+  }
+
+  approval = [{
+    type   = "group"
+    effect = "keep"
+    groups = [{
+      directory = "okta"
+      id        = "00example-group-id"
+      label     = "Example Okta Group"
+    }]
+  }]
+}
+
+resource "p0_routing_rule" "pagerduty_auto_approval" {
+  name = "example-pagerduty-auto-approval"
+
+  requestor = {
+    type = "any"
+  }
+
+  resource = {
+    type = "any"
+  }
+
+  approval = [{
+    type        = "auto"
+    integration = "pagerduty"
+    options = {
+      require_reason = true
+    }
+  }]
+}
+
+resource "p0_routing_rule" "escalation_service_approval" {
+  name = "example-escalation-service-approval"
+
+  requestor = {
+    type = "any"
+  }
+
+  resource = {
+    type = "any"
+  }
+
+  approval = [{
+    type        = "escalation"
+    integration = "pagerduty"
+    services    = ["security-escalation"]
+    options = {
+      require_preapproval = true
+    }
+  }]
+}
