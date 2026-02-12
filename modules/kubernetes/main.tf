301cc33d540a263b1529b64f49b41caaed7a9a70
Gus Boling <gus.boling@p0.dev>
2026-02-12 00:19:17 -0700

    Add roughed out terraform resources for an auto-mode k8s cluster

---
+++ modules/kubernetes/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    p0 = {
+      source = "p0-security/p0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Sets up authentication with the cluster
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      var.kubernetes.cluster_id,
+      "--region",
+      var.kubernetes.aws_region
+    ]
+  }
+}
